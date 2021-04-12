e8076ba392708f98565a0e8f186bddf7f4c8a063
khmaker <khmaker@yandex.ru>
2021-04-12 16:56:32 +0300

    autopep8 base files

--- Metamins_Bonus_account/wsgi.py
+++ Metamins_Bonus_account/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Metamins_Bonus_account.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'Metamins_Bonus_account.settings')
 
 application = get_wsgi_application()
