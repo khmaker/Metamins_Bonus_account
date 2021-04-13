0eee039b16cde8ec171c7ec71a675b2e168b8d27
khmaker <khmaker@yandex.ru>
2021-04-13 14:55:31 +0300

    errors.py InvalidAmount and InsufficientFunds classes

--- Bonus_account/errors.py
+++ Bonus_account/errors.py
@@ -1,1 +1,23 @@
 # coding=utf-8
+
+class Error(Exception):
+    pass
+
+
+class InvalidAmount(Error):
+
+    def __init__(self, amount):
+        self.amount = amount
+
+    def __str__(self):
+        return f'Invalid Amount: {self.amount}'
+
+
+class InsufficientFunds(Error):
+
+    def __init__(self, balance, amount):
+        self.balance = balance
+        self.amount = amount
+
+    def __str__(self):
+        return f'amount: {self.amount}, current balance: {self.balance}'
