4535aa090b9eb6c2ce341e06403f2ce76e7ef88a
khmaker <khmaker@yandex.ru>
2021-04-13 18:19:40 +0300

    admin.py AccountAdmin & OperationAdmin classes

--- Bonus_account/admin.py
+++ Bonus_account/admin.py
@@ -9,21 +9,38 @@ from .models import Operation
 class AccountAdmin(ModelAdmin):
     list_display = ('card_number', 'first_name', 'last_name', 'phone_number')
     search_fields = ('card_number', 'last_name', 'phone_number')
-    list_editable = ('first_name', 'last_name', 'phone_number')
 
     def has_delete_permission(self, request, obj=None):
         return True
 
+    def has_add_permission(self, request):
+        return False
+
+    def get_readonly_fields(self, request, obj=None):
+        return (
+            'card_number',
+            'phone_number',
+            'created',
+            'modified'
+            ) if obj is not None else ()
+
 
 class OperationAdmin(ModelAdmin):
-    list_display = ('account',
-                    'transaction_type',
-                    'bonus_operation_type',
-                    'created',
-                    )
+    list_display = (
+        'account',
+        'transaction_type',
+        'bonus_operation_type',
+        'created',
+        )
     list_filter = ('transaction_type', 'bonus_operation_type', 'created')
 
     def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
         return False
 
 
