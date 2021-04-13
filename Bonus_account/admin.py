05483173b050322ced7732a09d6520339cac477c
khmaker <khmaker@yandex.ru>
2021-04-13 17:20:19 +0300

    admin.py AccountAdmin & OperationAdmin classes

--- Bonus_account/admin.py
+++ Bonus_account/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin
+# coding=utf-8
+from django.contrib.admin import ModelAdmin
+from django.contrib.admin import site
 
-# Register your models here.
+from .models import Account
+from .models import Operation
+
+
+class AccountAdmin(ModelAdmin):
+    search_fields = ('card_number', 'last_name', 'phone_number')
+    list_editable = ('first_name', 'last_name', 'phone_number')
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+
+class OperationAdmin(ModelAdmin):
+    list_filter = ('transaction_type', 'bonus_operation_type', 'created')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+site.register(Account, AccountAdmin)
+site.register(Operation, OperationAdmin)
