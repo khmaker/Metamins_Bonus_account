3548527d42d1ad62cbd65b85e30df04d2473b62c
khmaker <khmaker@yandex.ru>
2021-04-15 11:32:36 +0300

    settings.py TEST_REQUEST_DEFAULT_FORMAT

--- Metamins_Bonus_account/settings.py
+++ Metamins_Bonus_account/settings.py
@@ -151,6 +151,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     }
 
 PHONENUMBER_DB_FORMAT = 'INTERNATIONAL'
